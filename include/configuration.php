a82e8551389223aa54a9c47b4cb5222ebd34589e
Devin Matte <Devinmatte@gmail.com>
2016-10-13 08:41:11 -0400

    Initial Commit as SubModule

---
+++ include/configuration.php
@@ -0,0 +1,11 @@
+<?
+	if (CONFIGURATION_INC == 1)
+        return;
+
+	define("CONFIGURATION_INC", 1);
+
+	define("CONF_ADMINID", "devinmatte");		/* Your MySQL userid */
+	define("CONF_ADMINPASS", "Gregfr0mcharter");		/* Your MySQL password in plain text */
+	define("CONF_LOCATION", "Localhost");	/* Location of database */
+	define("CONF_DATABASE", "GoogleCal");		/* Database to connect to */
+?>
