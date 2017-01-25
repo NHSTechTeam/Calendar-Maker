ebe137e7dce26315b6fd17600dfd3a3ed3c78cd7
Devin Matte <devinmatte@gmail.com>
2017-01-25 15:49:44 -0500

    Created Configuration Template

--- include/configuration-template.php
+++ include/configuration-template.php
@@ -5,7 +5,7 @@
 	define("CONFIGURATION_INC", 1);
 
 	define("CONF_ADMINID", "googlecal");		/* Your MySQL userid */
-	define("CONF_ADMINPASS", "Gregfr0mcharter");		/* Your MySQL password in plain text */
+	define("CONF_ADMINPASS", "password");		/* Your MySQL password in plain text */
 	define("CONF_LOCATION", "Localhost");	/* Location of database */
 	define("CONF_DATABASE", "GoogleCal");		/* Database to connect to */
 ?>
